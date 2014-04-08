3822d6287c7f4ac06d7dba761cef9e9186db12c7
joates <git@joat.es>
2014-04-09 00:18:17 +0100

    added textured floor image and sky-colored camera fog.

--- src/webgl.js
+++ src/webgl.js
@@ -7,7 +7,12 @@ var domready = require('domready')
   , scene    = new THREE.Scene()
   , camera   = new THREE.PerspectiveCamera(40, width / height, 0.1, 10000)
   , controls = new THREE.OrbitControls(camera)
-  , stats    = new Stats()
+//, stats    = new Stats()
+  , light    = new THREE.DirectionalLight(0xffffff, 1.5)
+
+  , floorTexture
+  , floorMaterial
+  , floorMesh
 
 module.exports = {
 
@@ -17,17 +22,26 @@ module.exports = {
                 var self = this
                 process.nextTick(function() {
                   domready(function() {
-                    scene.add(new THREE.AxisHelper(100))
-                    scene.add(new THREE.AmbientLight(0x202020))
+                    scene.add(light)
+                    scene.add(self.ground())
+
+                    //scene.add(new THREE.AxisHelper(100))
+
+                    scene.add(new THREE.AmbientLight(0xff9000))
+                    scene.fog = new THREE.Fog(0xaabfee, 3000, 8000)
+                    controls.maxPolarAngle = Math.PI / 2
                     camera.position.set(0, 250, 500)
 
                     self.renderer.setSize(width, height)
+                    self.renderer.setClearColor(scene.fog.color, 1)
                     document.body.appendChild(self.renderer.domElement)
 
+                    /*
                     stats.domElement.style.position = 'absolute'
                     stats.domElement.style.top  = '0px'
                     stats.domElement.style.left = '0px'
                     document.body.appendChild(stats.domElement)
+                    */
 
                     // async response.
                     cb(!scene instanceof THREE.Scene, scene)
@@ -43,10 +57,34 @@ module.exports = {
                 })
               }
 
+  , ground:   function() {
+                floorTexture = THREE.ImageUtils.loadTexture('img/floor.jpg')
+                floorTexture.repeat.set(0.5, 0.8)
+                floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping
+                floorMaterial  = new THREE.MeshPhongMaterial({
+                    ambient:   0x444444
+                  , color:     0x66aa66
+                  , shininess: 150
+                  , specular:  0x888888
+                  , shading:   THREE.SmoothShading
+                  , map:       floorTexture
+                })
+
+                floorMesh = new THREE.Mesh(new THREE.PlaneGeometry(16000, 16000), floorMaterial)
+                floorMesh.flipSided     = false
+                floorMesh.castShadow    = false
+                floorMesh.receiveShadow = true
+                floorMesh.rotation.x    = -Math.PI / 2
+                floorMesh.position.y    = -10
+
+                return floorMesh
+              }
+
   , update:   function(dt) {
-                camera.lookAt(scene.position)
+                light.position = camera.position.clone()
+                light.position.y += 5000
                 controls.update()
-                stats.update()
+                //stats.update()
               }
 
   , render:   function() {
